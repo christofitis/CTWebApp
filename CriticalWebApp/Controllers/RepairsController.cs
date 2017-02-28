58ac6d8ccacf2058091c6a5ac930221a657d4865
christofitis <chris13ryant@gmail.com>
2017-02-27 21:41:53 -0800

    Added name search with typeahead to repair controller/view

--- CriticalWebApp/Controllers/RepairsController.cs
+++ CriticalWebApp/Controllers/RepairsController.cs
@@ -13,9 +13,45 @@ namespace CriticalWebApp.Controllers
         private ApplicationDbContext _context = new ApplicationDbContext();
 
         // GET: Repairs sends a list of all repairs by date
-        public ActionResult Index()
+        public ActionResult Index(string searchCustomerName = null)
         {
+            if (!string.IsNullOrEmpty(searchCustomerName) && searchCustomerName.Length >= 2)
+            {
+                bool hasWhiteSpace = false;               
+                int counter = 0;
+                foreach (char c in searchCustomerName)
+                {
+                    if (Char.IsWhiteSpace(c))
+                    {
+                        hasWhiteSpace = true;
+                        break; 
+                    }
+                    counter++;
+                }
+                if (hasWhiteSpace)
+                {
+                    var customerFirstName = searchCustomerName.Remove(counter);
+                    var customerLastName = searchCustomerName.Substring(searchCustomerName.Length - (counter + 1));
+                    return View(_context.Repairs.Where(r => r.CustomerFirstName.Contains(customerFirstName) && r.CustomerLastName.Contains(customerLastName)).ToList());
+                }
+                else
+                {
 
+                    var repairs = _context.Repairs.Where(r => r.CustomerFirstName.Contains(searchCustomerName)).ToList();
+                    var lastNameQuery = _context.Repairs.Where(r => r.CustomerLastName.Contains(searchCustomerName)).ToList();
+                    foreach (var name in lastNameQuery)
+                    {
+                        repairs.Add(name);
+                    }
+                    
+                        //repairs = _context.Repairs.Where(r => r.CustomerFirstName.Contains(searchCustomerName)).ToList();
+                        return View(repairs);
+                    
+
+                }
+
+
+            }
             return View(_context.Repairs.ToList());
 
         }
