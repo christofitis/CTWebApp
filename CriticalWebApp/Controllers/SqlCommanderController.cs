c8bcf3e9a2958175c6255e34819e166974c166fc
christofitis <chris13ryant@gmail.com>
2017-03-31 11:38:53 -0700

    created sql commander

---
+++ CriticalWebApp/Controllers/SqlCommanderController.cs
@@ -0,0 +1,25 @@
+﻿using CriticalWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CriticalWebApp.Controllers
+{
+    public class SqlCommanderController : Controller
+    {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+        // GET: SqlCommander
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Run(string sqlCommand)
+        {
+            _context.Database.ExecuteSqlCommand(sqlCommand);
+            return RedirectToAction("Index");
+        }
+    }
+}
